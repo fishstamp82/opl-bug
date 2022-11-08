cdadd29377d18eed3c74162f7e3e22edd598d946
Ignat Harczuk <ignathe@gmail.com>
2022-11-08 11:29:31 +0100

    example of non working setup

---
+++ namespaces.keto.ts
@@ -0,0 +1,48 @@
+// Copyright © 2022 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
+// import { Namespace, SubjectSet, Context } from "@ory/keto-namespace-types"
+
+class User implements Namespace {
+  related: {
+    manager: User[]
+  }
+}
+
+class Group implements Namespace {
+  related: {
+    members: (User | Group)[]
+  }
+}
+
+class Folder implements Namespace {
+  related: {
+    parents: (File | Folder)[]
+    viewers: SubjectSet<Group, "members">[]
+  }
+
+  permits = {
+    view: (ctx: Context): boolean =>
+      this.related.viewers.includes(ctx.subject) ||
+      this.related.parents.traverse((p) => p.permits.view(ctx)),
+  }
+}
+
+class File implements Namespace {
+  related: {
+    parents: (File | Folder)[]
+    viewers: (User | SubjectSet<Group, "members">)[]
+    owners: (User | SubjectSet<Group, "members">)[]
+  }
+
+  // Some comment
+  permits = {
+    view: (ctx: Context): boolean =>
+      this.related.parents.traverse((p) => p.permits.view(ctx)) ||  // Will not parse this file due to the comment following the OR operator
+      this.related.viewers.includes(ctx.subject) ||
+      this.related.owners.includes(ctx.subject),
+
+    edit: (ctx: Context) => this.related.owners.includes(ctx.subject),
+  }
+}
+
